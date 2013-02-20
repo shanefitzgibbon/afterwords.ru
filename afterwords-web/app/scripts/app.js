643d94f527d59091a5fbacfa39e1dd2e20b7f310
Shane Fitzgibbon <shane.fitzgibbon@gmail.com>
2013-02-20 05:12:35 +0400

    Moved to AngularJS web framework and created basic main page layout.

---
+++ afterwords-web/app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var afterwords = angular.module('afterwords', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
